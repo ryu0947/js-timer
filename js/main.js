4a1e13735bfc6a750bb6b669c5d04e1a5a4988f7
ryu <dragonogawaz@gmail.com>
2020-12-10 21:16:00 +0900

    スタートボタンを押した時の処理を記述

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@
   const start = document.getElementById("start");
   const stop = document.getElementById("stop");
   const reset = document.getElementById("reset");
+  const comment = document.getElementById("commnt");
+
+  let startTime;
+
+  // Startボタンを押した時の処理
+  start.addEventListener("click", () => {
+    startTime = Date.now();
+    comment.textContent = "Working...";
+    countUp();
+  });
 }
